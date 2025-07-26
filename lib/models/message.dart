ef3394b17e0c3ff9058bff513174b1465e315fe2
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-26 14:36:02 +0300

    mod:n updated message model

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -18,4 +18,15 @@ class Message {
     required this.sender,
     required this.receiver,
   });
+ factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+      id: json['id'],
+      senderId: json['sender_id'],
+      receiverId: json['receiver_id'],
+      message: json['message'],
+      isRead: json['is_read'],
+      sender: User.fromJson(json['sender']),
+      receiver: User.fromJson(json['receiver']),
+    );
+  }
 }
